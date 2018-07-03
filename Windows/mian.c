f4ce17cddeff52a54db08f062dd02ed9c095d6b3
yaojunfeng <1773799663@qq.com>
2018-07-03 10:33:22 +0800

    my first commit

---
+++ Windows/mian.c
@@ -0,0 +1,11 @@
+#include<Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	WNDCLASS wc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+}
